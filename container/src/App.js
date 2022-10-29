0fb0421b9929034b08d2cca630e1fcdc6a8ada10
arunmishra-affinsys <Arun.Mishra@affinsys.com>
2022-10-29 14:47:12 +0530

    container

---
+++ container/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+// console.log(mount);
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hiii</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
+
+export default App;
