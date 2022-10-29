00463012939f5f83f43aaa54a50e69431a6fffed
arunmishra-affinsys <Arun.Mishra@affinsys.com>
2022-10-29 16:13:22 +0530

    CICD init

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin"); //injects js in html
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 const devConfig = {
   mode: "development",
   devServer: {
@@ -16,6 +17,8 @@ const devConfig = {
       remotes: {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
+      // shared: ["react", "react-dom"],
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 // console.log(mount);
 
-const App = () => {
+export default () => {
   return (
     <div>
       <h1>Hiii</h1>
@@ -11,5 +11,3 @@ const App = () => {
     </div>
   );
 };
-
-export default App;
